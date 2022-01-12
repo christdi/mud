cb4b1f55bbd6591a965a28aade57621058514ba1
Chris Irvine <chris@chrisirvine.co.uk>
2022-01-12 14:41:55 +0000

    Added new state table and test data

--- dist/db/01_base.sql
+++ dist/db/01_base.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS script (
   );
 
 INSERT INTO script VALUES('9f12ba01-d6c7-4e3d-bcff-0a2c92f91764', './command.lua', 1, 1, 1, 1, 1, 1);
+INSERT INTO script VALUES('96b6080b-5cf6-4781-89ee-e6602f9f274d', './login_state.lua', 1, 1, 1, 1, 1, 1);
 
 CREATE TABLE IF NOT EXISTS command (
   uuid TEXT PRIMARY KEY,
@@ -29,6 +30,18 @@ CREATE TABLE IF NOT EXISTS command (
 INSERT INTO command VALUES('1f8b5793-298f-4794-8557-4b851d668eb8', 'quit', 'do_quit', '9f12ba01-d6c7-4e3d-bcff-0a2c92f91764');
 INSERT INTO command VALUES('9456c374-81b6-49f7-8295-579001d629c0', 'shutdown', 'do_shutdown', '9f12ba01-d6c7-4e3d-bcff-0a2c92f91764');
 INSERT INTO command VALUES('fa479582-6465-44bd-8847-2b0971655706', 'script', 'do_script', '9f12ba01-d6c7-4e3d-bcff-0a2c92f91764');
+
+CREATE TABLE IF NOT EXISTS state (
+  uuid TEXT PRIMARY KEY,
+  name TEXT NOT NULL,
+  on_enter_function TEXT,
+  on_leave_function TEXT,
+  on_tick_function TEXT,
+  on_input_function TEXT,
+  script_uuid TEXT NOT NULL
+);
+
+INSERT INTO state VALUES('6c508205-b798-4ede-8353-619d47a0ed94', 'login', 'on_enter', 'on_leave', 'on_tick', 'on_input', '96b6080b-5cf6-4781-89ee-e6602f9f274d');
 
 CREATE TABLE IF NOT EXISTS entity (
   uuid TEXT PRIMARY KEY,
