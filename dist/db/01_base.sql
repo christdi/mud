e6f10027ad58a28c210e12e7a8e57675b2f7bd10
Christopher Irvine <chris@chrisirvine.co.uk>
2022-01-13 22:49:03 +0000

    Updated database schema, removed account, replaced with user

--- dist/db/01_base.sql
+++ dist/db/01_base.sql
@@ -1,10 +1,3 @@
-CREATE TABLE IF NOT EXISTS account (
-  username TEXT PRIMARY KEY,
-  password_hash TEXT NOT NULL
-  );
-
-INSERT INTO account VALUES('christdi','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
-
 CREATE TABLE IF NOT EXISTS script (
   uuid TEXT PRIMARY KEY,
   filepath TEXT NOT NULL,
@@ -15,7 +8,7 @@ CREATE TABLE IF NOT EXISTS script (
   allow_player_api INTEGER NOT NULL,
   allow_script_api INTEGER NOT NULL,
   allow_command_api INTEGER NOT NULL
-  );
+);
 
 INSERT INTO script VALUES('9f12ba01-d6c7-4e3d-bcff-0a2c92f91764', './command.lua', 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO script VALUES('96b6080b-5cf6-4781-89ee-e6602f9f274d', './login_state.lua', 1, 1, 1, 1, 1, 1, 1);
@@ -27,7 +20,7 @@ CREATE TABLE IF NOT EXISTS command (
   function TEXT NOT NULL,
   script_uuid TEXT NOT NULL,
   FOREIGN KEY(script_uuid) REFERENCES script(uuid)
-  );
+);
 
 INSERT INTO command VALUES('1f8b5793-298f-4794-8557-4b851d668eb8', 'quit', 'do_quit', '9f12ba01-d6c7-4e3d-bcff-0a2c92f91764');
 INSERT INTO command VALUES('9456c374-81b6-49f7-8295-579001d629c0', 'shutdown', 'do_shutdown', '9f12ba01-d6c7-4e3d-bcff-0a2c92f91764');
@@ -44,7 +37,7 @@ CREATE TABLE IF NOT EXISTS state (
   FOREIGN KEY(script_uuid) REFERENCES script(uuid)
 );
 
-CREATE UNIQUE INDEX idx_table_name ON state(name);
+CREATE UNIQUE INDEX idx_state_name ON state(name);
 
 INSERT INTO state VALUES('6c508205-b798-4ede-8353-619d47a0ed94', 'login', 'on_enter', 'on_exit', 'on_tick', 'on_input', '96b6080b-5cf6-4781-89ee-e6602f9f274d');
 INSERT INTO state VALUES('88461e14-cdc5-4092-9f96-bc7dc81f2b1d', 'play', 'on_enter', 'on_exit', 'on_tick', 'on_input', 'f019f74f-66ce-4e93-8ca5-7f8319f9fa1f');
@@ -53,16 +46,26 @@ CREATE TABLE IF NOT EXISTS entity (
   uuid TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   description TEXT NOT NULL
-  );
+);
 
 INSERT INTO entity VALUES('32f55645-f7fc-436a-ba9d-afb3c59d3654', 'Lithos', 'Code Monkey');
 
-CREATE TABLE IF NOT EXISTS account_entity (
-  account_username TEXT NOT NULL,
-  entity_uuid TEXT NOT NULL,
-  PRIMARY KEY(account_username, entity_uuid),
-  FOREIGN KEY(account_username) REFERENCES account (username),
-  FOREIGN KEY(entity_uuid) REFERENCES entity(uuid)
-  );
+CREATE TABLE IF NOT EXISTS user (
+  uuid TEXT PRIMARY KEY,
+  username TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
 
-INSERT INTO account_entity VALUES('christdi','32f55645-f7fc-436a-ba9d-afb3c59d3654');
+CREATE UNIQUE INDEX idx_user_username ON user(username);
+
+INSERT INTO user VALUES('1e4995dc-ddc7-4697-a8d4-76b6aa3939cc', 'christdi', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
+
+CREATE TABLE IF NOT EXISTS user_entity (
+  user_uuid TEXT NOT NULL,
+  entity_uuid TEXT NOT NULL,
+  PRIMARY KEY(user_uuid, entity_uuid),
+  FOREIGN KEY(user_uuid) REFERENCES user(uuid),
+  FOREIGN KEY(entity_uuid) REFERENCES entity(uuid)
+);
+
+INSERT INTO user_entity VALUES('1e4995dc-ddc7-4697-a8d4-76b6aa3939cc','32f55645-f7fc-436a-ba9d-afb3c59d3654');
